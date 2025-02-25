8fd7e4809b069afbaa98fd710c563e2e05d0453e
Skistua <b166er.dada@gmail.com>
2025-02-26 01:02:50 +0800

    [M] compatable with Win

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -22,8 +22,15 @@ return {
             },
         },
         -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-        build = "make",
-        -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+        build = (function()
+            local os_name = vim.loop.os_uname().sysname
+            if os_name == "Windows_NT" then
+                return "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+            else
+                return "make"
+            end
+        end)(),
+
         dependencies = {
             "nvim-treesitter/nvim-treesitter",
             "stevearc/dressing.nvim",
